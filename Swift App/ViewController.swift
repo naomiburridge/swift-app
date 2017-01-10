11d70f5ef9f7ef23845ee596b64647c91205e1b7
Naomi Burridge <naomi_burridge@yahoo.com>
2017-01-10 16:59:11 -0600

    My first commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Naomi Burridge on 1/10/17.
+//  Copyright © 2017 Naomi Burridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            coolLabel.text = "You tapped the button 10 times"
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+                }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
