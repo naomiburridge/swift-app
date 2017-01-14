fb65018d785b3f8d57c5f27717d1f55b7cebc01e
Naomi Burridge <naomi_burridge@yahoo.com>
2017-01-14 09:59:29 -0600

    no message

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var coolLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
@@ -20,20 +20,25 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: AnyObject) {
         
-        coolLabel.text = "Änswer: \(Double(text1.text!)! + Double(text2.text!)!)"
-        //coolLabel.text = String(Double(text1.text!)! + Double(text2.text!)!)
+        let addition = true
         
-            }
+        if addition {
+            coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+            //coolLabel.text = String(Double(text1.text!)! + Double(text2.text!)!)
+        } else {
+            coolLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-                }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
