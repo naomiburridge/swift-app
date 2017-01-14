818816196cdcd38fe6ef87b25241976530a5f7f1
Naomi Burridge <naomi_burridge@yahoo.com>
2017-01-14 10:01:08 -0600

    Revert "no message"
    This reverts commit fb65018d785b3f8d57c5f27717d1f55b7cebc01e.

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var coolLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
@@ -20,25 +20,20 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: AnyObject) {
         
-        let addition = true
+        coolLabel.text = "Änswer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        //coolLabel.text = String(Double(text1.text!)! + Double(text2.text!)!)
         
-        if addition {
-            coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
-            //coolLabel.text = String(Double(text1.text!)! + Double(text2.text!)!)
-        } else {
-            coolLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
-        }
-    }
+            }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+                }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+
 }
 
